c6ce99ef7bd86e3d9c238ad77464f72a661a345e
kongwl <13092371318@163.com>
2024-12-04 16:42:36 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
     }
 }
