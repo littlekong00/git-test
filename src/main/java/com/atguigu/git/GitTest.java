89d262ab13b6719df8d2c12a747f1d64fe9ccac8
kongwl <13092371318@163.com>
2024-12-04 11:56:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author littleKong00
+ * @time 2024-12-03-15:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
